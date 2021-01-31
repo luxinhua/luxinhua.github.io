7b91e21a275d8c29fca8bf9d9f68546a1fb177a9
xhlu <2846154596@qq.com>
2021-02-01 00:38:39 +0800

    update article.

---
+++ gen-article.sh
@@ -0,0 +1,9 @@
+git fetch source_dev
+
+git branch source_dev source_dev/main
+
+git branch -r 
+
+git checkout branch_dev
+
+hexo new post $1
---
+++ push-github.sh
@@ -0,0 +1,7 @@
+hexo clean 
+hexo g
+
+git add .
+git commit -a -m "update new article." 
+git remote -v 
+git push source_dev HEAD
