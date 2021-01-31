d56e5c393b7fcb565bfc47407194f1d196b2d0aa
xhlu <2846154596@qq.com>
2021-02-01 01:14:43 +0800

    update article.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 rm -rf ../public
 mkdir ../public
 cp ./public/* ../public -rf 
-git checkout -b source_dev origin/source_dev
+git checkout source_dev
 cp ../public/* . -rf
 git add .
 git commit -a -m "update article."
