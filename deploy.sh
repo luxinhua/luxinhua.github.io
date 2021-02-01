f9ae3d92c4363664df1710438486aabbfd538b1a
xhlu <2846154596@qq.com>
2021-02-01 20:41:31 +0800

    update source code.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ git checkout -b source_dev origin/source_dev
 git checkout -b resp_md_file origin/resp_md_file
 git checkout main
 
-hexo clean 
+# hexo clean 
 hexo g
 npm install --save hexo-deployer-git
 hexo d 
