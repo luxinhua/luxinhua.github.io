3284eec02fcf243b199b2c70d0c7f7b1124c8a13
xhlu <2846154596@qq.com>
2021-02-01 20:05:24 +0800

    update source code.

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,9 @@ git add .
 git commit -a -m "update source code."
 git push origin main
 
-rm -rf ../tmpxhlupublic
 rm -rf ../tmpxhlumdfile
-mkdir ../tmpxhlupublic
 mkdir ../tmpxhlumdfile
-cp ./public/* ../tmpxhlupublic -rf 
 cp ./source/_posts/* ../tmpxhlumdfile -rf 
-
-git checkout source_dev
-rm -rf *
-cp ../tmpxhlupublic/* . -rf
-git add .
-git commit -a -m "update article."
-git push origin source_dev
 
 git checkout resp_md_file
 rm -rf *
@@ -26,3 +16,5 @@ git commit -a -m "update article."
 git push origin resp_md_file
 
 git checkout main
+
+hexo d
