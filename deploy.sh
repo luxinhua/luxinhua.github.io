83c93980fb83a46e8734f47d43e12fa5f8ee8f9f
xhlu <2846154596@qq.com>
2021-02-01 20:51:05 +0800

    update source code.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
-git checkout -b source_dev origin/source_dev
-git checkout -b resp_md_file origin/resp_md_file
-git checkout main
-
-# hexo clean 
+hexo clean 
 hexo g
 npm install --save hexo-deployer-git
 hexo d 
@@ -10,16 +6,3 @@ hexo d
 git add .
 git commit -a -m "update source code."
 git push origin main
-
-rm -rf ../tmpxhlumdfile
-mkdir ../tmpxhlumdfile
-cp ./source/_posts/* ../tmpxhlumdfile -rf 
-
-git checkout resp_md_file
-rm -rf *
-cp ../tmpxhlumdfile/* . -rf 
-git add .
-git commit -a -m "update article."
-git push origin resp_md_file
-
-git checkout main
